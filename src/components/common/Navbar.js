a7d4d2317f4fab894f169b830da3a4bc4c98be0b
DJG-coderslab <coderslab.sjg@pwr.pl>
2021-10-17 14:50:22 +0200

    Component BasicMenu

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,25 +1,14 @@
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import BasicMenu from "./BasicMenu";
 
 const Navbar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Button color="inherit">Login</Button>
+              <Toolbar>
+                  <BasicMenu />
         </Toolbar>
       </AppBar>
     </Box>
